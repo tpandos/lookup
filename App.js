b3d03263ec31ea77303b37b4524bc024e4d19373
tee <t.pandos@protonmail.com>
2021-03-01 20:44:33 -0500

    connection front-back-end

--- App.js
+++ App.js
@@ -1,42 +1,19 @@
-
-import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { fetchMeetups } from './constants/api';
-import Sandbox from './components/sandbox';
-import SignUp from './screens/registratrion';
+import React, {Component} from 'react';
+import Router from './app/router'
 
-export default function App() {
-  const test_data = useState();
-
-  const all_data = async () => {
-    const datas = await fetchMeetups()
-    console.log('datas', datas)
+export default class App extends Component {
+  render() {
+      return <Router/>;
   }
-  
-  useEffect(()=>{
-    all_data()
-  },[])
- 
-  return (
-    //<Sandbox />
-    <View style={styles.container}>
-      <SignUp />
-        <View>
-          
-          <View>
-            <Text>Blank</Text>
-         
-          </View>
-        </View>
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0a031d',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
