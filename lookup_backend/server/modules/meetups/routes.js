43633e7dcc96484e96b88c86caf776a7c3d6aa04
yifengjin89 <55426354+yifengjin89@users.noreply.github.com>
2021-03-02 02:13:05 -0500

    working frontend

--- lookup_backend/server/modules/meetups/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import * as MeetupController from './controller'
-
-const routes = new Router()
-
-routes.post('/meetups', MeetupController.createMeetup)
-routes.get('/meetups', MeetupController.getAllMeetups)
-
-export default routes
