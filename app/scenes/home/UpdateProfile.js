8beefeb2b4a948a9f2f05b4607a3185f40c45493
tpandos <t.pandos@gmail.com>
2021-03-21 03:48:26 -0400

    still can't get the array to be send to db

--- app/scenes/home/UpdateProfile.js
+++ app/scenes/home/UpdateProfile.js
@@ -26,12 +26,12 @@ export default function UpdateProfile (props) {
         {name: 'major', label: 'Major', required: true},
         {name: 'grade', label: 'Grade Level', required: true},
         
-        {  name: 'skills_1', label: 'Skills-1'},
-        {  name: 'skills_2', label: 'Skills-2'},
+        {  name: 'name', label: 'Skills-1'},
         // {  name: 'skills-3', label: 'Skills-3'},
        
-        {  name: 'rank_1', label: 'Rank-1'},
-        {  name: 'rank_2', label: 'Rank-2'}, 
+        {  name: 'rank', label: 'Rank-1'},
+     
+  
         // {  name: 'rank-3', label: 'Rank-3'},
      
      
@@ -43,7 +43,7 @@ export default function UpdateProfile (props) {
         try {
             let response = await api.updateProfile(state.user._id, data);
             updateUser(response.user);
-
+            console.log("this is from upprof: ", data);
             setLoading(false);
 
             navigation.goBack();
--- app/services/auth.js
+++ app/services/auth.js
@@ -21,7 +21,7 @@ export async function login(data){
         throw handler(e);
     }
 }
-
+                                                                               
 export async function forgotPassword(data) {
     try {
         let res = await axios.post(c.FORGOT_PASSWORD, data);
@@ -37,45 +37,85 @@ export async function updateProfile(userId, data){
         const options = {
             headers: {
                 Accept: "application/json",
-                "Content-Type": "multipart/form-data"
+                "Content-Type": "multipart/form-data",
+                
             }
+            
         };
         console.log('data==========================', data)
-        console.log('datalength', data.skills_1)
+        
+        console.log("skills.name ------", data.name); 
+        console.log("skills.rank ------", data.rank); 
 
-        let skills = [];
-        let rank = [];
-        let i = 1;
-        for (let key in data) {
-            console.log('key', key)
-            if (i > 3) return;
-            if (key == `skills_${i}`) {
-                for (let key_2 in data) {
-                    if (key_2 == `rank_${i}`) {
-                        console.log('find it ===============')
-                        console.log('value', data[key])
-                        skills.push(data[key])
-                        rank.push(data[key_2])   
-                    }
-                }
-                delete data.skills_1
-                delete data.rank_1
-                i++;
-            }
+      
+
+        // let skills = [];
+        // let rank = [];
+        // let i = 1;
+
+   
+
+        // for (let key in data) {
+        //     console.log('key', key)
+        //     if (i > 3) return;
+        //     if (key == `skills_${i}`) {
+        //         for (let key_2 in data) {
+        //             if (key_2 == `rank_${i}`) {
+        //                 console.log('find it ===============')
+        //                 console.log('value', data[key])
+        //                 skills.push(data[key])
+        //                 rank.push(data[key_2])   
+        //             }
+        //             console.log('key_2', key_2)
+        //         }
+        //         delete data.skills_1
+        //         delete data.rank_1
+        //         i++;
+        //     }
                
-        }
-        console.log('skills ================', skills)
-        console.log('rank:===================', rank)
+        // }
+
+        // console.log('skills ================', skills)
+        // console.log('rank:===================', rank)
 
         const form_data = new FormData();
         console.log('type of form_data', form_data)
         console.log('data==================')
         console.log(data)
-        for ( let key in data )
-            form_data.append(key, data[key]);
+        // for ( let key in data ){
+        //     form_data.append(key, data[key]);
+        // }
 
+       
+        // create object using the data for skills 
+
+        let name = data.name; 
+        let rank = data.rank; 
+      
+        let skills = ["noooooooo","88"]; 
+       
+        // let skills2 = {"name": data.name2, "rank": data.rank2}
+        // console.log("these are the skilzzzzzz")
+        // console.log(skills); 
+
+        // form_data.append("institute", data['institute']);
+        // form_data.append("major", data['major']);
+        // form_data.append("role", data['role']);
+        // form_data.append("username", data['username']);
+        // form_data.append("grade", data['grade']);
+        form_data.append('skills', skills); 
+  
+    
         console.log('formdata=====================')
         console.log(form_data)
+
+        console.log('object keys');
+        // let somekeys = Object.keys(skills); 
+        // let somekeys2 = Object.keys(skills2); 
+        // console.log(somekeys);
+        // console.log(somekeys2);
+
+
         let res = await axios.put(`${c.UPDATE_PROFILE}/${userId}`, form_data, options);
 
         return res.data;
