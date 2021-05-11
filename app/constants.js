853e93b8d7a4051e7e0a84965777168e694b4ef8
albert-tadros <albert.tadros4@gmail.com>
2021-05-10 22:54:00 -0400

    no changes happened

--- app/constants.js
+++ app/constants.js
@@ -4,7 +4,7 @@ import React from 'react';
 export const API_URL = 'https://lookupbackend.herokuapp.com/api';
 
 //USING LOCAL
-//export const API_URL = 'http://192.168.188:3000/api';
+//export const API_URL = 'http://192.168.1.3:3000/api';
 
 //API End Points
 export const REGISTER = `${API_URL}/auth/register`;
@@ -13,3 +13,4 @@ export const UPDATE_PROFILE = `${API_URL}/user`;
 export const UPLOAD_IMAGE = `${API_URL}/user/upload`;
 export const FORGOT_PASSWORD = `${API_URL}/auth/recover`;
 export const SEARCH = `${API_URL}/user`;
+export const SEARCHED_PROFILE = `${API_URL}/user`;
--- app/scenes/home/Search.js
+++ app/scenes/home/Search.js
@@ -1,8 +1,7 @@
-import React, {useState, useContext, useEffect} from 'react';
+import React, {useState, useSafeState, useContext, useEffect, useRef} from 'react';
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { Component } from 'react'
 import { FlatList, SafeAreaView, ActivityIndicator, ScrollView, StyleSheet, Text, View} from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/Ionicons'
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import * as Animatable from 'react-native-animatable'
@@ -10,9 +9,10 @@ import * as api from "../../services/auth";
 import { useAuth } from "../../providers/auth";
 import Form, { TYPES } from 'react-native-basic-form';
 import {ErrorText} from "../../components/Shared";
+import axios from 'axios';
+import * as c from '../../constants';
 
 
-//const ListItems = ['C++', 'Js', 'Bussiness', 'IOS development']
 
 export default function Search (props) {
   const {navigation} = props;
@@ -21,7 +21,14 @@ export default function Search (props) {
 const [error, setError] = useState(null);
 const [loading, setLoading] = useState(false);
 const {state, updateUser } = useAuth();
-const [res, setData] = useState([])
+updateUser(state.user)
+const [formData, setFormData] = useState();
+const [userData , setData] = useState({userData});
+
+//const [isCancelled, setCancel] = useState(true);
+// const cancelSource = useRef(false)
+// cancelSource.current = axios.CancelToken.source();
+const [mount , setMount] = useState(false);
 
 
 const options = [
@@ -42,51 +49,47 @@ const fields = [
   mode: 'dialog',
   required: true}]
 
-  //const initialData = {"method" : 1};
+
+
+function onSubmit (data) {
+    setMount(true);
+    setFormData(data)
   
-   async function onSubmit (data) {
-    //useEffect (async () => {
-    const {navigation} = props;
-    const {navigate} = props.navigation;
+}
+
+useEffect(() => {
+if (mount == true ){
+
+    console.log("state.user===")
+    //console.log(state.user);
+    console.log(state.user._id);
+
+async function fetchApi(){
     setLoading(true);
-    
       try {
-        let response = await api.search(state.user._id, data);
-        console.log('response');
-        console.log(response)
-        console.log(response.results.length)
-        //updateUser(response.user);
+        const response = await api.search(state.user._id, formData);
         setLoading(false);
+
+
+        await AsyncStorage.setItem( "userResponse" , JSON.stringify(response));
+        props.navigation.navigate('SearchResults')
         
-        const UsernameArray = response.results.map((item) => {
-          return item.username
-        }); 
-        //console.log(UsernameArray.length);
-        //console.log(UsernameArray);
-
-        const ProfileImageArray = response.results.map((item) => {
-          return item.profileImage
-        }); 
-        //console.log(ProfileImageArray.length);
-        //console.log(ProfileImageArray);
-
-        const RankingArray = response.results.map((item) => {
-          return item.ranking
-        }); 
-        //console.log(RankingArray.length);
-        //console.log(RankingArray);
         
-       navigate('SearchResults', {Usernames : UsernameArray , ProfileImages: ProfileImageArray, Ranking : RankingArray})
-
-    } 
-    
-    catch (error) {
+      }catch (error) {
         setError(error.message);
         setLoading(false)
-    }
+        }
+        //setMount(false)
+      }
+      fetchApi();
   }
+    return () => { 
+        setMount(false)
+    }
+}, [formData])
+  
 
-        
+
   return(
   <View>
     <ScrollView>
@@ -101,13 +104,14 @@ const fields = [
                 fields={fields}
                 title={'Search'}
                 loading={loading}
-                initialData={state.user}
+                initialData={null}
                 error={error}
+                //style = {{backgroundColor: '#6D25BE',  marginTop:2, marginLeft: 2, padding: 10, width: 150, borderRadius: 30, borderColor:'#fff', borderWidth: '2'}}
                 onSubmit={onSubmit}/>
             </Animatable.View>
             <TouchableOpacity
             style={{backgroundColor: '#6D25BE', alignItems: 'center',  marginTop:2, marginLeft: 2, padding: 10, width: 150, borderRadius: 30, borderColor:'#fff', borderWidth: '2'}}
-            onPress={()=>{navigate('Home')}}> 
+            onPress={()=>{navigate('App')}}> 
             <Text style={{ textAlign: 'center', color: '#fff', fontSize: 15}}> Back to Home</Text>
             </TouchableOpacity>
             
@@ -119,62 +123,7 @@ const fields = [
         
     )
   }
-            /*<FlatList 
-                    res = {ListItems}
-                    keyExtractor = {(item,index) => index.toString()}
-                    renderItem = {({item} ) => 
-                    <TouchableOpacity
-                    style={{backgroundColor: '#37474f', marginTop:2, marginLeft: 10, padding: 200, width: 150, borderRadius: 30, marginHorizontal: 2, borderColor:'#fff', borderWidth: '2'}}>
-                    <Text style={{ textAlign: 'left', color: '#fff', fodntSize: 18}}> {item}</Text>
-                    </TouchableOpacity>
-                    }
-                  />*/
-                  /*<View style= {styles.container}>
-                  <View style={{flex:1, padding:50}}>
-                      <ErrorText error={error}/>
-                      <Animatable.View animation = "slideInRight" duration= {1000} style= {{width: 380, height: 250, backgroundColor: 'white', 
-                      flexDirection:'row', padding: 5, alignItems: 'center', paddingRight:5, borderRadius: 1}}>
-                        <Animatable.View animation = "fadeInLeft" duration = {400}> 
-                        </Animatable.View>
-                        <Form
-                          fields={fields}
-                          title={'Submit'}
-                          loading={loading}
-                          initialData={state.user}
-                          error={error}
-                          onSubmit={onSubmit}/>
-                      </Animatable.View>
-                      <View>
-                      <FlatList 
-                              respo = {res}
-                              keyExtractor = {(item,index) => index.toString()}
-                              renderItem = {({item} ) => 
-                              <TouchableOpacity
-                              style={{backgroundColor: '#37474f', marginTop:2, marginLeft: 10, padding: 200, width: 150, borderRadius: 30, marginHorizontal: 2, borderColor:'#fff', borderWidth: '2'}}
-                              onPress= {() => navigation.navigate('SearchResults', item)}
-                              >
-                              <Text style={{ textAlign: 'left', color: '#fff', fodntSize: 18}}> {item}</Text>
-                              
-                              </TouchableOpacity>
-                              }
-                            />
-                        </View>
-                      <TouchableOpacity
-                      style={{backgroundColor: '#6D25BE', alignItems: 'center',  marginTop:2, marginLeft: 2, padding: 10, width: 150, borderRadius: 30, borderColor:'#fff', borderWidth: '2'}}
-                      onPress={()=>{navigate('Home')}}> 
-                      <Text style={{ textAlign: 'center', color: '#fff', fontSize: 15}}>Back to Home</Text>
-                      </TouchableOpacity>
-                      
-                  </View>
-              </View>
-             </View>
-          
-                  
-              )
-            }*/
  
- 
-
       const styles = StyleSheet.create({
         container: {
           flex: 1,
